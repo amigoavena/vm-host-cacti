f0e311f0ae21daa3c0a6cf1449e66b37ed8c9d31
Hector Avena <amigoavena@gmail.com>
2013-08-20 13:59:27 +0900

    Update vi_host_vmfs.pl
    Fixed to display vmfs usage correctly.

--- vi_host_vmfs.pl
+++ vi_host_vmfs.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
 
-$host_vmfs_usage = `perl /var/www/cacti/scripts/check_vmware.pl --server $ARGV[0] --username $ARGV[1] --password $ARGV[2] --instance $ARGV[3] --report disk|awk -F \"\\(|%\" '{print \$2}'`;
+$host_vmfs_usage = `perl /usr/share/cacti/site/scripts/check_vmware.pl --server $ARGV[0] --username $ARGV[1] --password $ARGV[2] --instance $ARGV[3] --report disk| head -1 `;
 
-printf("percent:%.1f\n", $host_vmfs_usage);
+chomp($host_vmfs_usage);
+
+my @vmInfo= split('\|',$host_vmfs_usage);
+
+my ($vmfsFree,$vmfsTotal) = ($vmInfo[1] =~ m/free=(\d+);;;0; total=(\d+);;;0;/);
+
+$percent = ($vmfsFree/$vmfsTotal)*100;
+
+printf("percent:%.1f\n", $percent);
